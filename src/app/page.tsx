7b33c7bedc7899dde06b395cc33ffedca69a6706
Jeff Barragan <leshz@me.com>
2024-04-20 10:11:04 -0500

    schema and implementation banners

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import DoubleBanner from '../components/banner/DoubleBanner'
+import { DoubleBanner } from '../components/banner/double-banner'
 import ChooseProduct from '../components/Home/ChooseProduct'
 import BestSellingProduct from '../components/Home/BestSellingProduct'
 import JustForSection from '../components/Home/JustForSection'
@@ -14,19 +14,30 @@ import Newsletters from '../components/Home/Newsletter'
 import InstagramSection from '../components/Home/InstagramSection'
 import PromoModal from '../components/common/PromoModal'
 import LatestArticle from '../components/home2/LatestArticle'
+import { Suspense } from 'react'
 
+async function getPageInfo() {
+  const res = await fetch('http://127.0.0.1:1337/api/home?populate=*')
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+  return res.json()
+}
 
-export default function Home() {
-
-
+export default async function Home() {
+  const {
+    data: { attributes }
+  } = await getPageInfo()
   return (
     <main>
-      {/* <DoubleBanner />
-      <ChooseProduct />
-      <ExclusiveProduct />
+      <Suspense>
+        <DoubleBanner data={attributes} />
+      </Suspense>
+      {/* <ChooseProduct /> */}
+      {/* <ExclusiveProduct />
       <LatestArticle />
       <Testimonial />
-      <InstagramSection /> */}
+      <InstagramSection />  */}
     </main>
   )
 }
--- src/components/banner/double-banner.tsx
+++ src/components/banner/double-banner.tsx
@@ -1,11 +1,67 @@
-import React, { useMemo } from 'react'
+'use client'
+import { useMemo } from 'react'
 import Link from 'next/link'
 import SwiperCore from 'swiper'
 import { Autoplay, EffectFade, Navigation, Pagination } from 'swiper/modules'
 import { Swiper, SwiperSlide } from 'swiper/react'
 SwiperCore.use([Pagination, Autoplay, EffectFade, Navigation])
+import {
+  BlocksRenderer,
+  type BlocksContent
+} from '@strapi/blocks-react-renderer'
 
-const Banner2 = () => {
+const FixedBanner = ({ content }) => {
+  const { title } = content
+  return (
+    <div className="col-xxl-3 col-xl-4">
+      <div className="banner-2-left">
+        <div className="banner-2-left-img">
+          <img src="/assets/img/home2/banner-2-left-img.png" alt="" />
+        </div>
+        <div className="banner-2-left-content">
+          <BlocksRenderer
+            content={title}
+            blocks={{
+              // You can use the default components to set class names...
+              paragraph: ({ children }) => (
+                <h2 className="text-neutral900 max-w-prose">{children}</h2>
+              ),
+              // ...or point to a design system
+              heading: ({ children, level }) => {
+                switch (level) {
+                  case 1:
+                    return <h2 className="">{children}</h2>
+                  case 2:
+                    return <h2 className="">{children}</h2>
+                  case 3:
+                    return <h2 className="">{children}</h2>
+                  case 4:
+                    return <h2 className="">{children}</h2>
+                  case 5:
+                    return <h2 className="">{children}</h2>
+                  case 6:
+                    return <h2 className="">{children}</h2>
+                  default:
+                    return <h2 className="">{children}</h2>
+                }
+              }
+            }}
+            modifiers={{
+              bold: ({ children }) => <strong>{children}</strong>
+            }}
+          />
+          <Link href="/shop" className="primary-btn3 hover-btn5">
+            Shop Now
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const DoubleBanner = ({ data }) => {
+  const { fixed_banner } = data
+
   const bannerSlide = useMemo(() => {
     return {
       slidesPerView: 1,
@@ -27,21 +83,7 @@ const Banner2 = () => {
     <div className="banner-2-section mb-110 ">
       <div className="container-fluid p-0">
         <div className="row">
-          <div className="col-xxl-3 col-xl-4">
-            <div className="banner-2-left">
-              <div className="banner-2-left-img">
-                <img src="/assets/img/home2/banner-2-left-img.png" alt="" />
-              </div>
-              <div className="banner-2-left-content">
-                <h2>
-                  Up to <strong>40%</strong> off All Makeup
-                </h2>
-                <Link legacyBehavior href="/shop">
-                  <a className="primary-btn3 hover-btn5">Shop Now</a>
-                </Link>
-              </div>
-            </div>
-          </div>
+          <FixedBanner content={fixed_banner} />
           <div className="col-xxl-9 col-xl-8">
             <div className="banner-2-right">
               <Swiper {...bannerSlide} className="swiper banner2-slider">
@@ -157,4 +199,4 @@ const Banner2 = () => {
   )
 }
 
-export default Banner2
+export { DoubleBanner }
