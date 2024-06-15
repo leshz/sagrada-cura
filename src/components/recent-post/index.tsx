33d979ddafb590ba74d01af6e2484b91e7aeeb72
Jeff Barragan <leshz@me.com>
2024-06-14 21:45:40 -0500

    Post recientes

--- src/components/recent-post/index.tsx
+++ src/components/recent-post/index.tsx
@@ -1,67 +1,47 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
+import { getColletions } from '@/services'
+import { COLLECTIONS } from '@/utils/constants'
 import Link from 'next/link'
+import { dateFormat } from '@/utils/helpers'
+import { ImageWrapper } from '@/components/Image'
 
-const RecentPost = () => (
+const RecentPost = async () => {
+  const params = {
+    sort: 'publishedAt:asc',
+    'pagination[limit]': '3'
+  }
+  const { data = [] } = await getColletions(COLLECTIONS.blogs, { params })
+
+  return (
     <div className="shop-widget mb-30">
-      <h5 className="shop-widget-title">Recent Post</h5>
-      <div className="recent-post-widget mb-20">
-        <div className="recent-post-img">
-          <Link legacyBehavior href="/blog-details">
-            <a>
-              <img src="/assets/img/inner-page/recent-post-img1.png" alt="" />
-            </a>
-          </Link>
-        </div>
-        <div className="recent-post-content">
-          <Link legacyBehavior href="/blog-grid">
-            <a>20 July, 2023</a>
-          </Link>
-          <h6>
-            <Link legacyBehavior href="/blog-details">
-              <a>Poutsicle Hydrating Lipo Stain offering.</a>
-            </Link>
-          </h6>
-        </div>
-      </div>
-      <div className="recent-post-widget mb-20">
-        <div className="recent-post-img">
-          <Link legacyBehavior href="/blog-details">
-            <a>
-              <img src="/assets/img/inner-page/recent-post-img2.png" alt="" />
-            </a>
-          </Link>
-        </div>
-        <div className="recent-post-content">
-          <Link legacyBehavior href="/blog-grid">
-            <a>12 July, 2023</a>
-          </Link>
-          <h6>
-            <Link legacyBehavior href="/blog-details">
-              <a>rutrum vitae augue idel euismod pulvi.</a>
-            </Link>
-          </h6>
-        </div>
-      </div>
-      <div className="recent-post-widget mb-20">
-        <div className="recent-post-img">
-          <Link legacyBehavior href="/blog-details">
-            <a>
-              <img src="/assets/img/inner-page/recent-post-img3.png" alt="" />
-            </a>
-          </Link>
-        </div>
-        <div className="recent-post-content">
-          <Link legacyBehavior href="/blog-grid">
-            <a>04 July, 2023</a>
-          </Link>
-          <h6>
-            <Link legacyBehavior href="/blog-details">
-              <a>Donec blandit fermentu diam non.</a>
-            </Link>
-          </h6>
-        </div>
-      </div>
+      <h5 className="shop-widget-title">Post recientes</h5>
+      {data.map(({ id, title, image, slug, publishedAt }) => {
+        const link = `/blog/${slug}`
+        return (
+          <div key={id} className="recent-post-widget mb-20">
+            <div className="recent-post-img">
+              <Link href={link}>
+                <ImageWrapper
+                  image={image}
+                  width={90}
+                  height={75}
+                  format="thumbnail"
+                />
+              </Link>
+            </div>
+            <div className="recent-post-content">
+              <Link href={link}>
+                <time>{dateFormat(publishedAt)}</time>
+              </Link>
+              <h6>
+                <Link href={link}>{title}</Link>
+              </h6>
+            </div>
+          </div>
+        )
+      })}
     </div>
   )
+}
 
 export { RecentPost }
