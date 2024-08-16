36af7f6136df4d47da53ae72e52ac1b42491f8bb
Jeff Barragan <leshz@me.com>
2024-08-16 07:13:08 -0500

    Fix: style improvements

--- src/components/highlight-product/index.tsx
+++ src/components/highlight-product/index.tsx
@@ -88,7 +88,7 @@ const HightLights = ({ highlights }) => {
                             </Link>
                           </div>
                         </div>
-                        <div className="col-lg-6 d-none d-sm-block">
+                        <div className="col-lg-6 d-none d-sm-none d-lg-block">
                           <div className="exclusive-product-right">
                             <div className="product-right-img hover-img">
                               {/* <Link href="/shop/product-default"> */}
