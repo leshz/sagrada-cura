9feeb8280e42816dc50ea6954ba831d2cfff11b8
Jeff Barragan <leshz@me.com>
2024-09-02 20:16:50 -0500

    Fix build

--- src/app/etiquetas/page.tsx
+++ src/app/etiquetas/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from 'react'
 import type { Metadata } from 'next'
 import Link from 'next/link'
 import { getColletions, getSingles } from '@/services'
@@ -39,44 +40,46 @@ const TagsCatalog = async () => {
   const hasProducts = data?.length >= 1
 
   return (
-    <div className="right-sidebar-section column-sidebar-padding mt-50 mb-40">
-      <div className="container-fluid">
-        <div className="row gy-5 justify-content-center">
-          <div className="col-xl-9 order-xl-1 order-2">
-            {hasProducts && (
-              <div className="all-products list-grid-product-wrap">
-                <div className="shop-columns-title-section mb-40">
-                  <p>{`${total} Productos`}</p>
-                  <div className="filter-selector">
-                    <GridSelector />
+    <Suspense>
+      <div className="right-sidebar-section column-sidebar-padding mt-50 mb-40">
+        <div className="container-fluid">
+          <div className="row gy-5 justify-content-center">
+            <div className="col-xl-9 order-xl-1 order-2">
+              {hasProducts && (
+                <div className="all-products list-grid-product-wrap">
+                  <div className="shop-columns-title-section mb-40">
+                    <p>{`${total} Productos`}</p>
+                    <div className="filter-selector">
+                      <GridSelector />
+                    </div>
+                  </div>
+                  <div className="row gy-4 mb-80 ">
+                    {data?.map(product => (
+                      <Card
+                        key={product.id}
+                        product={product}
+                        labels={labels}
+                        isTag
+                      />
+                    ))}
                   </div>
                 </div>
-                <div className="row gy-4 mb-80 ">
-                  {data?.map(product => (
-                    <Card
-                      key={product.id}
-                      product={product}
-                      labels={labels}
-                      isTag
-                    />
-                  ))}
+              )}
+              {!hasProducts && (
+                <div className="all-products list-grid-product-wrap">
+                  <div className="p-2 mb-40 text-center">
+                    <h1 className="mb-40">Sin productos disponibles </h1>
+                    <Link href="/tienda" className="primary-btn1 hover-btn3">
+                      Limpiar filtros
+                    </Link>
+                  </div>
                 </div>
-              </div>
-            )}
-            {!hasProducts && (
-              <div className="all-products list-grid-product-wrap">
-                <div className="p-2 mb-40 text-center">
-                  <h1 className="mb-40">Sin productos disponibles </h1>
-                  <Link href="/tienda" className="primary-btn1 hover-btn3">
-                    Limpiar filtros
-                  </Link>
-                </div>
-              </div>
-            )}
+              )}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Suspense>
   )
 }
 
