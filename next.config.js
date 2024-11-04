ef935826d769afb22d1996e25690f7c6ea358c09
Jeff Barragan <leshz@me.com>
2024-11-04 18:18:57 -0500

    add  CSP

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const { redirect } = require('next/dist/server/api-utils')
+const { headers } = require('next/headers')
 const path = require('path')
+
+const prodCSP = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.googletagmanager.com https://va.vercel-scripts.com;
+  style-src 'self' 'unsafe-inline';
+  connect-src 'self' https://analytics.google.com/;
+  img-src 'self' https://www.googletagmanager.com ${process.env.CDN};
+  font-src 'self' data:;
+  frame-src 'self';
+`
+  .trim()
+  .replace(/(\r\n|\n|\r)/g, '')
+
+const contentSecurityPolicy = process.NODE_ENV === 'production' ? prodCSP : ''
 
 const nextConfig = {
   reactStrictMode: false,
@@ -25,6 +40,23 @@ const nextConfig = {
     fetches: {
       fullUrl: false
     }
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: contentSecurityPolicy
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN'
+          }
+        ]
+      }
+    ]
   },
   async redirects() {
     return [
