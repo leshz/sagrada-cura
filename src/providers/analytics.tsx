c0022a64dd34a247fd0a60a9f4128333811198bb
Jeff Barragan <leshz@me.com>
2025-04-07 02:26:21 +0000

    chore: update analitycs dependency

--- src/providers/analytics.tsx
+++ src/providers/analytics.tsx
@@ -1,7 +1,7 @@
 import { GoogleAnalytics } from '@next/third-parties/google'
 
 const Analytics = () => (
-  <GoogleAnalytics gaId={`${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`} />
+  <GoogleAnalytics gaId={`${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`} debugMode={process.env.NODE_ENV === 'development'} />
 )
 
 export { Analytics }
